269bf47ab32e5796bc179acd599acbb4b2094f71
Craig Bowditch <craigbowditch@hotmail.com>
2018-08-12 00:19:35 +0100

    initial commit, creates database

---
+++ db/plant_shop.sql
@@ -0,0 +1,18 @@
+DROP TABLE plants;
+DROP TABLE species;
+
+CREATE TABLE species(
+id SERIAL PRIMARY KEY,
+name VARCHAR(255),
+description TEXT
+);
+
+CREATE TABLE plants(
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255),
+  description TEXT,
+  stock_quantity INT,
+  buying_cost INT,
+  selling_price INT,
+  species_id INT REFERENCES species(id) ON DELETE CASCADE
+);
