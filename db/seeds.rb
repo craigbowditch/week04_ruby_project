018631f8e50041814e085ef71cb2f2e43e41c5ae
Craig Bowditch <craigbowditch@hotmail.com>
2018-08-12 01:39:29 +0100

    Creates save method for plant class and tests

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,6 @@
 require_relative('sql_runner.rb')
 require_relative('../models/species.rb')
+require_relative('../models/plants.rb')
 
 require ('pry')
 
@@ -12,6 +13,17 @@ maranta = Species.new(
     "description" => "Maranta is a genus of flowering plants in the family Marantaceae, native to tropical Central and South America and the West Indies."
     })
     maranta.save()
+
+plant1 = Plant.new(
+  {
+    "name" => "Red Prayer Plant",
+    "latin_name" => "Maranta leuconeura var. erythroneura",
+    "description" => "Red prayer plant shows off dark green leaves, purple markings, and rich red veins.",
+    "stock_quantity" => 4,
+    "buying_cost" => 5,
+    "selling_price" => 10
+    })
+    plant1.save
 
 
     binding.pry
---
+++ models/plants.rb
@@ -0,0 +1,42 @@
+require_relative ('../db/sql_runner.rb')
+
+class Plant
+  attr_reader :id
+  attr_accessor :name, :latin_name, :description, :stock_quantity, :buying_cost, :selling_price
+
+  def initialize(options)
+    @id = options["id"].to_i() if options["id"]
+    @name = options["name"]
+    @latin_name = options["latin_name"]
+    @description = options["description"]
+    @stock_quantity = options["stock_quantity"]
+    @buying_cost = options["buying_cost"]
+    @selling_price = options["selling_price"]
+  end
+
+  def save()
+    sql = "INSERT into plants (name, latin_name, description, stock_quantity, buying_cost, selling_price) VALUES ($1, $2, $3, $4, $5, $6) RETURNING id"
+    values = [@name, @latin_name, @description, @stock_quantity, @buying_cost, @selling_price]
+    result = SqlRunner.run(sql, values).first
+    @id = result["id"].to_i()
+  end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+end
