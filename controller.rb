b17277535489c73e400754aacc6dcf764460f164
Craig Bowditch <craigbowditch@hotmail.com>
2018-08-12 22:36:50 +0100

    Creates index route for plants

---
+++ controller.rb
@@ -0,0 +1,13 @@
+require ('sinatra')
+require ('sinatra/contrib/all') if development?
+require ('pry-byebug')
+
+require_relative ('models/plants.rb')
+require_relative ('models/species.rb')
+also_reload ('models/*')
+
+#INDEX
+get '/plant-shop/plants' do
+  @plants = Plant.all()
+  erb (:plant_index)
+end
