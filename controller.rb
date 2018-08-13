6999645c82b2c918a9d8140109be2ffd31b2f579
Craig Bowditch <craigbowditch@hotmail.com>
2018-08-13 10:04:15 +0100

    makes create root for plants

--- controller.rb
+++ controller.rb
@@ -4,6 +4,8 @@ require ('pry-byebug')
 
 require_relative ('models/plants.rb')
 require_relative ('models/species.rb')
+require_relative ('./species_controller.rb')
+
 also_reload ('models/*')
 
 #INDEX
@@ -14,7 +16,15 @@ end
 
 #NEW
 get '/plant-shop/plants/new' do
+  @species = Species.all()
   erb (:new)
+end
+
+#CREATE
+post '/plant-shop/plants' do
+  @plant = Plant.new(params)
+  @plant.save()
+  erb (:create)
 end
 
 #SHOW
